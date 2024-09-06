30e6f160677c279708508de0e7c038b161d3e22c
pontifex <93377002+PontifexMatthew@users.noreply.github.com>
2024-09-06 10:26:41 +0900

    class

---
+++ First_time_to_use_class.py
@@ -0,0 +1,11 @@
+class Puppy:
+  def __init__(self):
+    self.name = "dubu"
+    self.age = 1
+    self.breed = "Persian cat"
+
+dubu = Puppy()
+print(dubu.name, dubu.age, dubu.breed)
+
+hodu = Puppy()
+print(hodu.name, hodu.age, hodu.breed)
